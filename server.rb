81942530c162e74f11dcbd2fa46a15bdf2d08c42
Emma Follender <efollender@gmail.com>
2014-08-06 12:49:20 -0500

    Added get methods

--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 # required gem includes
 require 'sinatra'
 require "sinatra/json"
-require_relative 'lib/sesh.rb'
+#require_relative 'lib/sesh.rb'
 
 set :bind, '0.0.0.0' # Vagrant fix
 set :sessions, true
@@ -9,3 +9,11 @@ set :session_secret, 'super secret'
 
 # partial
 # layouts
+
+get '/' do
+  erb :index
+end
+
+get '/signup' do
+  erb :signup
+end
