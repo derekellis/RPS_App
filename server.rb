40b9a435480d2ee4600e711388fb12d77b0828a2
Emma Follender <efollender@gmail.com>
2014-08-06 17:06:46 -0500

    Added get methods for new layouts. Included scripts for specific layouts

--- server.rb
+++ server.rb
@@ -11,9 +11,38 @@ set :session_secret, 'super secret'
 # layouts
 
 get '/' do
+  @home = 'js/home.js'
   erb :index
 end
 
-get '/signup' do
-  erb :signup
+get '/matches' do
+  @js = 'js/pushmenu.js'
+  @matches = nil#Sesh.dbi.get_all_matches(username)
+  erb :matches
+end
+
+get '/signin' do
+  user = Sesh.dbi.get_user_by_username(params['username'])
+  if user && user.has_password?(params['password'])
+    session['sesh_example'] = user.username
+    redirect to '/matches'
+  else
+    "THAT'S NOT THE RIGHT PASSWORD!!!!"
+  end
+end
+
+get '/play' do
+  @audio = 'js/audio.js'
+  @active_user = true
+  @current_player = 'Player 1'
+  erb :play
+end
+
+get '/signout' do
+  session.clear
+  redirect to '/'
+end
+
+post '/signup' do
+  redirect to '/'
 end
